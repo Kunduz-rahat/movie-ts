eb83030d28b77ec081b78f6377c7e4819c40efa3
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-02-06 18:30:09 +0600

    add movie item action

--- src/store/actions/movieActions.ts
+++ src/store/actions/movieActions.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from "redux";
 import axios from "axios";
-import { MovieListAction, MovieListActionTypes } from "../../types/movieTypes";
+import { MovieItemAction, MovieItemActionTypes, MovieListAction, MovieListActionTypes } from "../../types/movieTypes";
 import apiConfig from "../../api/apiConfig.js";
 
 export const fetchMovies = () => {
@@ -20,6 +20,26 @@ export const fetchMovies = () => {
       .catch((error) => {
         dispatch({
           type: MovieListActionTypes.FETCH_MOVIE_LIST_ERROR,
+          payload: { error: "Произошла ошибка при загрузке фильмов" },
+        });
+      });
+  };
+};
+
+
+export const fetchItemMovie = (id:number) => {
+  return (dispatch: Dispatch<MovieItemAction>) => {
+    dispatch({type:MovieItemActionTypes.FETCH_MOVIE_ITEM_REQUEST})
+    axios(`${apiConfig.baseUrl}movie/${id}?api_key=${apiConfig.apiKey}`)
+      .then(({ data }) => {
+        dispatch({
+          type: MovieItemActionTypes.FETCH_MOVIE_ITEM_SUCCESS,
+          payload: data,
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: MovieItemActionTypes.FETCH_MOVIE_ITEM_ERROR,
           payload: { error: "Произошла ошибка при загрузке фильмов" },
         });
       });
